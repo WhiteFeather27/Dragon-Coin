4b2315ac769524ca4b21faeb436b7b12ebffc91c
WhiteFeather2772 <WhiteFeather@NirvanaLifestyleInc.onmicrosoft.com>
2021-09-13 22:56:18 -0500

    greaT

--- Dragon Official/SourceCODE DGN.js
+++ Dragon Official/SourceCODE DGN.js
@@ -1,14 +1,16 @@
 const request = require("sync-request");
 
 module.exports = function(callback) {
-    const account = web3.personal.newAccount("123456789");
-    console.log(`Created ETH wallet to deploy contracts: ${account} (password is '123456789', don't loose it)`);
-    console.log(`Requesting 1 ETH to ${account} (http://faucet.ropsten.be:3001/donate/${wss
-        :; //eth-ropsten.alchemyapi.io/v2/o93me4joIgLBJZ_b7E1ROZJYj4x7_hha})`)
-    const res = request("GET", `http://faucet.ropsten.be:3001/donate/${wss
-        :; //eth-ropsten.alchemyapi.io/v2/o93me4joIgLBJZ_b7E1ROZJYj4x7_hha}`)
-    if (res.statusCode >= 300) {
-        throw new Error(`Sorry, Ethereum test faucet request failed: ${res.body.toString()}`);
+
+    const account = web3.personal.newAccount("WhiteBuffaloNAC");
+    console.log(`Created ETH wallet to deploy contracts: ${WhiteFeather2772} (password is '123456789', don't loose it)`);
+    console.log(`Requesting 1 ETH to ${WhiteFeather2772} ({"wss:/eth-mainnet.alchemyapi.io/v2/d3gcNdAu3nfdmBVrarbfjGkScvnGeCvz"})
+        :; "wss://eth-mainnet.alchemyapi.io/v2/d3gcNdAu3nfdmBVrarbfjGkScvnGeCvz")
+    const res = request("GET", `{"wss:/eth-mainnet.alchemyapi.io/v2/d3gcNdAu3nfdmBVrarbfjGkScvnGeCvz"}
+        : ; "wss://eth-mainnet.alchemyapi.io/v2/d3gcNdAu3nfdmBVrarbfjGkScvnGeCvz")`)
+    if (res.statusCode >= 300) 
+        {"wss:/eth-mainnet.alchemyapi.io/v2/d3gcNdAu3nfdmBVrarbfjGkScvnGeCvz"}
+        throw new Error ${res.body.toString()}`);
     }
     console.log("Requested, waiting on the Ethereum node to sync and then 15 seconds to receive test ETH...");
     setTimeout(() => {
@@ -17,7 +19,7 @@ module.exports = function(callback) {
         15 * 1000);}
 
 function checkBalance(account, callback) {
-    const balance = web3.eth.getBalance(wss://eth-ropsten.alchemyapi.io/v2/o93me4joIgLBJZ_b7E1ROZJYj4x7_hha)
+    const balance = web3.eth.getBalance= ("wss:/eth-mainnet.alchemyapi.io/v2/d3gcNdAu3nfdmBVrarbfjGkScvnGeCvz")
   if (balance.toNumber() > 0) {
         console.log(`
     Received;
@@ -28,7 +30,7 @@ function checkBalance(account, callback) {
     is;
     $;
     {
-        web3.fromWei(balance, "ether")
+        web3.fromWei(balance,  "DGN")
     }
     ETH);`);
 callback(); } else
@@ -39,30 +41,17 @@ callback(); } else
         },
         15 * 1000);
 }
-}
-pragma;
-solidity ^ 0.5;
-.8;
-
-// Imports
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
-
-// Main token smart contract
-contract;
-DGNToken;
-is;
-ERC20Mintable;
 {
-    string;
-    public;
-    constant;
-    name = "Dragon Coin";
-    string;
-    public;
-    constant;
-    symbol = "DGN";
-    uint8;
-    public;
-    constant;
-    decimals = 0.0000000000000000000001;
-}
+    
+    pragma solidity ^0.5.8;
+
+    // Imports
+    import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+    
+    // Main token smart contract
+    contract DGNToken is ERC20Mintable
+      string public constant name = "Dragon Coin";
+      string public constant symbol = "DGN";
+      uint8 public constant decimals = 0.0000000000000000000001;
+    }
+    
